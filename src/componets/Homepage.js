176d0a6e9a20d0e50e73d2bb9d34e86451fa76f5
Kirill Kruchenok <spinagrblzzz@gmail.com>
2020-06-12 18:11:54 +0300

    feat: fetching show list implemented

--- src/componets/Homepage.js
+++ src/componets/Homepage.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Homepage.css";
 
+const axios = require("axios").default;
+
 function Homepage() {
+	const [list, setList] = useState([]);
+	const listNode = React.createRef();
+
+	useEffect(() => {
+		axios.get("http://api.tvmaze.com/schedule/full").then((responce) => {
+			setList(responce.data);
+		});
+	}, []);
+
 	return (
 		<div className="homepage">
 			<fieldset>
 				<input type="submit" value="SUBMIT" />
 			</fieldset>
-			<ul>
-				<li>Placeholder</li>
+			<ul className="list" ref={listNode}>
+				{list.length === 0 ? (
+					<p>no match</p>
+				) : (
+					list
+						.slice(0, 10)
+						.map((listItem) => (
+							<li key={listItem.id}>{listItem.name}</li>
+						))
+				)}
 			</ul>
 		</div>
 	);
