2ed3dfb6c929ea4834a50a400c8fb99f615634d1
Kirill Kruchenok <spinagrblzzz@gmail.com>
2020-06-16 10:59:28 +0300

    feat: favorite page added

---
+++ src/componets/FavoriteItem.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function FavoriteItem({ name }) {
+	return <li>{name}</li>;
+}
+
+export default FavoriteItem;
--- src/componets/FavoritesPage.js
+++ src/componets/FavoritesPage.js
@@ -1,15 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import addToFavorites from "../redux/addToFavorites";
 import removeFromfavorites from "../redux/removeFromfavorites";
+import FavoriteItem from "./FavoriteItem";
+
+const axios = require("axios").default;
 
 function FavoritesPage({ favorites, addToFavorites, removeFromfavorites }) {
+	const [list, setList] = useState(null);
+	useEffect(() => {
+		//Promise.all(favorites.map(({tvrage, thetvdb, imdb})=>axios.get())
+		//itog
+		//`http://api.tvmaze.com/lookup/shows?${tvrage?`tvrage=${tvrage}`: thetvdb?`thetvdb=${thetvdb}`:`imdb=${imdb}`}`
+		Promise.all(
+			favorites.map(({ tvrage, thetvdb, imdb }) =>
+				axios.get(
+					`http://api.tvmaze.com/lookup/shows?${
+						tvrage
+							? `tvrage=${tvrage}`
+							: thetvdb
+							? `thetvdb=${thetvdb}`
+							: `imdb=${imdb}`
+					}`
+				)
+			)
+		).then((responses) => {
+			console.log(responses);
+			setList(responses.map((response) => response.data));
+		});
+	}, []);
+
 	return favorites.length === 0 ? (
 		<p>no favorites added</p>
-	) : (
+	) : list ? (
 		<ul>
-			<p>some list</p>
+			{list.map((favItem) => (
+				<FavoriteItem name={favItem.name} />
+			))}
 		</ul>
+	) : (
+		<h1>W8</h1>
 	);
 }
 
