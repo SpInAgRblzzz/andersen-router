79b00d8419caed9cf632db8ea9b3ea2f77ae2473
Kirill Kruchenok <spinagrblzzz@gmail.com>
2020-06-16 13:27:07 +0300

    feat: content fetch added

--- src/componets/ShowPage.js
+++ src/componets/ShowPage.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+const axios = require("axios").default;
 
 function ShowPage() {
-	return <div>paaaage</div>;
+	const { id } = useParams();
+	const [content, setContent] = useState(null);
+	useEffect(() => {
+		axios.get(`http://api.tvmaze.com/shows/${id}`).then((response) => {
+			console.log("response", response.data);
+			setContent(response.data);
+		});
+	}, []);
+	return content ? (
+		<div>
+			<img src={content.image.medium} />
+		</div>
+	) : (
+		<h1>W8</h1>
+	);
 }
 
 export default ShowPage;
