6ed5221a3cee77c8fcbd7ffe34ac22da62d98e98
Kirill Kruchenok <spinagrblzzz@gmail.com>
2020-06-16 17:08:00 +0300

    feat: description added

--- src/componets/ShowPage.js
+++ src/componets/ShowPage.js
@@ -6,16 +6,19 @@ const axios = require("axios").default;
 function ShowPage() {
 	const { id } = useParams();
 	const [content, setContent] = useState(null);
+
 	useEffect(() => {
 		axios.get(`http://api.tvmaze.com/shows/${id}`).then((response) => {
 			console.log("response", response.data);
 			setContent(response.data);
 		});
 	}, []);
+
 	return content ? (
 		<div>
 			<img src={content.image.medium} alt={`${content.name} show`} />
 			<FavoriteButton id={id} />
+			<p>{content.summary.replace("<p>", "").replace("</p>", "")}</p>
 		</div>
 	) : (
 		<h1>W8</h1>
